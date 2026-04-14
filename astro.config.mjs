fe4ade702a6154d3e9b54cc1f51b05004ae9accd
Manavarya09 <smanavarya@gmail.com>
2026-04-14 08:20:47 +0400

    fix: Inline CSS styles for landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://manavarya09.github.io',
-  base: '/HELIOS',
-  vite: {
-    plugins: [tailwindcss()]
+  build: {
+    inlineStylesheets: 'always'
   }
 });
